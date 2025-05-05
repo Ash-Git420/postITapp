9f9dc9102d93c236e0ae59bc5b1587b11a31d6f4
Ash-Git420 <66S2136294@utas.edu.om>
2025-05-05 11:09:05 +0400

    5/5/2025

--- client/src/Components/User.js
+++ client/src/Components/User.js
@@ -1,16 +1,18 @@
 import user from "../Images/user.png";
 import { useSelector } from "react-redux";
+import Location from "./Location.js";
 
 const User = () => {
   const email = useSelector((state) => state.users.user.email);
   const name = useSelector((state) => state.users.user.name);
   return (
     <div>
-      <img src={user} className="userImage" />
+      <img src={user} alt="" className="userImage" />
       <p>
         {name}
         <br />
         {email}
+        <Location />
       </p>
     </div>
   );
